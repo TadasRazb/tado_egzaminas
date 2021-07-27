db7d848734061bc1d35fe4349fe42cb5687157c9
tadas.razb <tadas.razb@gmail.com>
2021-07-27 10:24:03 +0300

    sukurtas spring projektas

---
+++ src/main/java/tado_egz/TadoEgzaminasApplication.java
@@ -0,0 +1,13 @@
+package tado_egz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadoEgzaminasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadoEgzaminasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tado_egz/TadoEgzaminasApplicationTests.java
@@ -0,0 +1,13 @@
+package tado_egz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadoEgzaminasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
